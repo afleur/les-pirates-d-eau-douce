3686a8e56410a4efd66af5432069280f0b7fad83
Alois Fleuricourt <afleuricourt@gmail.com>
2019-02-18 16:30:53 +0100

    add boat

---
+++ app/controllers/boats_controller.rb
@@ -0,0 +1,36 @@
+class BoatsController < ApplicationController
+  # before_action :set_boats, only: :show
+
+  def new
+    @user = User.find(params[:user_id])
+    @boat = Boat.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @boat = Boat.new(params_boat)
+    @boat.user = @user
+    if @boat.save
+      redirect_to boats_path(@boat), notice: 'Boat was successfully added.'
+    else
+      render :new
+    end
+  end
+
+  def index
+    @boats = Boat.all
+  end
+
+  # def show
+  # end
+
+  private
+
+  def set_boats
+    @boat = Boat.find(params[:id])
+  end
+
+  def params_boat
+    params.require(:boat).permit(:name, :category, :price_per_day, :description, :picture, :address, :user_id)
+  end
+end
