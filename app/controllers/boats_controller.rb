1db3140560fc1b3118ed92eaecd6600b1beb2532
Gieri Cosson <gieri@infomaniak.ch>
2019-02-19 14:44:15 +0100

    all stories working

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,6 +1,6 @@
 class BoatsController < ApplicationController
   before_action :set_boats, only: :show
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def new
     @boat = Boat.new
